05ab70eb6d5a79b8a18a3d51ed6a0d017c25eb42
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2024-12-03 18:21:17 -0500

    initial integration test

--- pynimcodec/cbc/message/base_message.py
+++ pynimcodec/cbc/message/base_message.py
@@ -1,13 +1,16 @@
 """Compact Binary Codec Message class and methods."""
 
-from pynimcodec.bitman import append_bytes_to_buffer, extract_from_buffer
+from pynimcodec.bitman import append_bytes_to_buffer
 from pynimcodec.cbc.codec.base_codec import CbcCodec, CodecList
 from pynimcodec.cbc.constants import MessageDirection
-from pynimcodec.cbc.field.base_field import Field, Fields, encode_fields
+from pynimcodec.cbc.field.base_field import Fields, encode_fields, decode_fields
+from pynimcodec.cbc.field.common import create_field
 
 
 class Message(CbcCodec):
     """A message data structure."""
+    
+    required_args = ['name', 'direction', 'message_key', 'fields']
     
     def __init__(self,
                  name: str,
@@ -15,16 +18,16 @@ class Message(CbcCodec):
                  message_key: int,
                  fields: Fields,
                  **kwargs) -> None:
-        super().__init__(name, kwargs.pop('description'))
+        super().__init__(name, kwargs.pop('description', None))
         self._direction: MessageDirection = None
-        self.direction = direction
         self._message_key: int = None
-        self.message_key = message_key
-        self._fields: Fields = Fields()
-        self.fields = fields
+        self._fields: Fields = None
         self._coap_compatible: bool = kwargs.get('coap_compatible', True)
         self._vsat_reserved: bool = kwargs.get('vsat_reserved', False)
         self._nimo_compatible: bool = kwargs.get('nimo_compatible', False)
+        self.direction = direction
+        self.message_key = message_key
+        self.fields = fields
     
     @property
     def direction(self) -> MessageDirection:
@@ -70,18 +73,39 @@ class Messages(CodecList[Message]):
         super().__init__(Message)
 
 
-def decode(buffer: bytes, message: Message, **kwargs) -> dict:
+def create_message(obj: dict) -> Message:
+    """Creates a Message from a dictionary definition."""
+    if not isinstance(obj, dict):
+        raise ValueError('Invalid object to create Message.')
+    if 'messageKey' in obj:
+        obj['message_key'] = obj.pop('messageKey')
+    if not all(k in obj for k in Message.required_args):
+        raise ValueError(f'Missing required argument ({Message.required_args})')
+    valid_msgdir = [e.value for e in MessageDirection]
+    if not isinstance(obj['direction'], MessageDirection):
+        if obj['direction'] not in valid_msgdir:
+            raise ValueError(f'Invalid Message direction ({valid_msgdir})')
+        obj['direction'] = MessageDirection(obj['direction'])
+    if not isinstance(obj['fields'], list):
+        raise ValueError('Invalid fields definition')
+    for i, field in enumerate(obj['fields']):
+        obj['fields'][i] = create_field(field)
+    obj['fields'] = Fields(obj['fields'])
+    return Message(**obj)
+
+
+def decode_message(buffer: bytes, message: Message, **kwargs) -> dict:
     """Decodes a Message from a data buffer.
     
     Which Message codec to use is determined by parsing either the first two
-    bytes of raw over-the-air data received, or the 3rd-4th bytes if using
+    bytes of Non-IP over-the-air data received, or the 3rd-4th bytes if using
     Constrained Application Protocol (CoAP) transport.
     
     Args:
         buffer (bytes): The data buffer containing the message payload after
-            removing the raw or CoAP header.
+            removing the NIDD or CoAP header.
         message (Message): The message codec to use for decoding.
-        **raw (bool): Indicates the buffer begins with the 2-byte message_key.
+        **nim (bool): Indicates the buffer begins with the 2-byte message_key.
         **coap (bool): Indicates the buffer includes CoAP header.
     
     Returns:
@@ -92,9 +116,9 @@ def decode(buffer: bytes, message: Message, **kwargs) -> dict:
     if not isinstance(message, Message):
         raise ValueError('Invalid message codec')
     offset = 0
-    if kwargs.get('raw') is True:
+    if kwargs.get('nim') is True:
         if kwargs.get('coap') is True:
-            raise ValueError('raw and coap are mutually exclusive flags')
+            raise ValueError('nim and coap are mutually exclusive flags')
         offset = 16
         message_key = int.from_bytes(buffer[0:2])
         if message_key != message.message_key:
@@ -105,49 +129,38 @@ def decode(buffer: bytes, message: Message, **kwargs) -> dict:
     decoded = { 'name': message.name }
     if message.description:
         decoded['description'] = message.description
-    decoded['fields'] = []
-    for field in message.fields:
-        if field.optional is True:
-            present = extract_from_buffer(buffer, offset, 1)
-            offset += 1
-            if not present:
-                continue
-        decoded_field, offset = field.decode(buffer, offset)
-        decoded['fields'].append(decoded_field)
+    decoded['value'], _ = decode_fields(message, buffer, offset)
+    return decoded
 
 
-def encode(content: dict, message: Message, **kwargs) -> 'bytes':
+def encode_message(content: dict, message: Message, **kwargs) -> 'bytes':
     """Encodes message content using a message definition.
     
     The data bytes produced should be appended to either the first two bytes of
-    raw over-the-air data or as the CoAP MessageID (3rd-4th bytes).
+    Non-IP over-the-air data or as the CoAP MessageID (3rd-4th bytes).
     
     Args:
         content (dict): The message content data structure.
         message (Message): The message codec to use for encoding.
-        **raw (bool): Include the message_key as the first 2 bytes for transport.
+        **nim (bool): Include the message_key as the first 2 bytes for transport.
         **coap (bool): Create a CoAP message using message_key as MessageID
     
     Returns:
         bytes: The data buffer for payload (without message_key header bytes)
     """
-    # Embedded helper function
-    def get_content_field(name: str) -> 'Field|None':
-        for cfield in content['fields']:
-            if cfield['name'] == name:
-                return cfield
-    # Main function
-    if (not isinstance(content, dict) or 'fields' not in content
-        or not all(isinstance(f, dict) for f in content['fields'])):
+    if (not isinstance(content, dict) or 'value' not in content
+        or not all(isinstance(f, dict) for f in content['value'])):
         raise ValueError('Invalid content to encode.')
     if not isinstance(message, Message):
         raise ValueError('Invalid message codec.')
     buffer = bytearray()
     offset = 0
-    if kwargs.get('raw') is True:
+    if kwargs.get('nim') is True:
         if kwargs.get('coap') is True:
-            raise ValueError('raw and coap are mutually exclusive flags')
-        buffer = append_bytes_to_buffer(message.message_key.to_bytes(2, 'big'))
+            raise ValueError('nim and coap are mutually exclusive flags')
+        buffer = append_bytes_to_buffer(message.message_key.to_bytes(2, 'big'),
+                                        buffer,
+                                        offset)
         offset = 16
     if kwargs.get('coap') is True:
         raise NotImplementedError
