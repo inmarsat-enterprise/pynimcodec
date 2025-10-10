80357d646ef1424d85cf9d2b6cb6db7d5b01edcf
geoffbrucepayne <geoff.bruce-payne@inmarsat.com>
2025-10-10 09:56:47 -0400

    refactor and improve tests

--- tests/test_bitman.py
+++ tests/test_bitman.py
@@ -1,12 +1,9 @@
-import math
-
 import pytest
 
 from pynimcodec.bitman import BitArray, is_int, extract_from_buffer, append_bits_to_buffer, append_bytes_to_buffer
 
 
 def test_is_int():
-    """"""
     assert is_int(1) is True
     assert is_int(-1) is True
     assert is_int(2**64) is True
@@ -16,7 +13,6 @@ def test_is_int():
 
 
 def test_extract_from_buffer():
-    """"""
     test_buffer = bytes.fromhex('010203')
     assert extract_from_buffer(test_buffer, 0, 8) == test_buffer[0]
     assert extract_from_buffer(test_buffer, 8, 8, as_buffer=True) == test_buffer[1:2]
@@ -24,15 +20,13 @@ def test_extract_from_buffer():
 
 
 def test_append_bits_to_buffer():
-    """"""
     test_buffer = bytes.fromhex('01')
-    extended = append_bits_to_buffer(BitArray(1), test_buffer, len(test_buffer) * 8)
+    extended = append_bits_to_buffer(BitArray([1]), test_buffer, len(test_buffer) * 8)
     assert len(extended) == len(test_buffer) + 1
     assert extended[len(extended) - 1] == 0b10000000
 
 
 def test_append_bytes_to_buffer():
-    """"""
     test_buffer = bytes.fromhex('01')
     new_bytes = bytes.fromhex('01')
     extended = append_bytes_to_buffer(new_bytes, test_buffer, len(test_buffer) * 8)
@@ -43,17 +37,15 @@ def test_append_bytes_to_buffer():
 
 
 def test_bitarray_create():
-    """"""
     bit_array = BitArray()
     assert isinstance(bit_array, BitArray)
-    bit_array = BitArray(1,0,1)
+    bit_array = BitArray([1,0,1])
     assert isinstance(bit_array, BitArray)
     with pytest.raises(ValueError):
-        bit_array = BitArray(2)
+        bit_array = BitArray([2])
 
 
 def test_bitarray_append():
-    """"""
     bit_array = BitArray()
     assert len(bit_array) == 0
     bit_array.append(1)
@@ -63,7 +55,6 @@ def test_bitarray_append():
 
 
 def test_bitarray_extend():
-    """"""
     bit_array = BitArray()
     bit_array.extend([1,0,1])
     assert len(bit_array) == 3
@@ -72,16 +63,14 @@ def test_bitarray_extend():
 
 
 def test_bitarray_insert():
-    """"""
-    bit_array = BitArray(0,1)
+    bit_array = BitArray([0,1])
     bit_array.insert(0,1)
     assert len(bit_array) == 3
     assert bit_array[0] == 1
 
 
 def test_bitarray_setitem_getitem():
-    """"""
-    bit_array = BitArray(1,0)
+    bit_array = BitArray([1,0])
     bit_array[1] = 1
     assert bit_array[1] == 1
     with pytest.raises(ValueError):
@@ -89,34 +78,31 @@ def test_bitarray_setitem_getitem():
 
 
 def test_bitarray_delitem():
-    """"""
-    bit_array = BitArray(1,1,1)
+    bit_array = BitArray([1,1,1])
     del bit_array[0]
     assert len(bit_array) == 2
 
 
 def test_bitarray_repr():
-    """"""
-    bit_array = BitArray(1,0,1)
+    bit_array = BitArray([1,0,1])
     assert repr(bit_array) == 'BitArray([1, 0, 1])'
 
 
 def test_bitarray_str():
-    """"""
-    bit_array = BitArray(1,0,1)
+    bit_array = BitArray([1,0,1])
     assert str(bit_array) == '0b101'
 
 
-def test_bitarray_from_int():
-    """"""
-    test_val = 5
+@pytest.mark.parametrize('test_val', [-1, 0, 5, -5])
+def test_bitarray_from_int(test_val):
     bit_array = BitArray.from_int(test_val)
     assert isinstance(bit_array, BitArray)
-    assert len(bit_array) == math.ceil(math.log2(test_val))
+    # special case: 0 has bit_length() = 0, but still uses at least 1 bit
+    expected_len = max(1, test_val.bit_length() + (1 if test_val < 0 else 0))
+    assert len(bit_array) == expected_len
 
 
 def test_bitarray_from_bytes():
-    """"""
     test_val = bytes.fromhex('010203')
     bit_array = BitArray.from_bytes(test_val)
     assert isinstance(bit_array, BitArray)
@@ -124,7 +110,6 @@ def test_bitarray_from_bytes():
 
 
 def test_bitarray_read_int():
-    """"""
     test_val = 101
     bit_array = BitArray.from_int(test_val)
     assert bit_array.read_int() == test_val
@@ -135,7 +120,6 @@ def test_bitarray_read_int():
 
 
 def test_bitarray_read_bytes():
-    """"""
     test_val = bytes.fromhex('010203')
     bit_array = BitArray.from_bytes(test_val)
     assert bit_array.read_bytes() == test_val
@@ -145,7 +129,6 @@ def test_bitarray_read_bytes():
 
 
 def test_bitarray_lshift():
-    """"""
     test_val = 15
     bit_array = BitArray.from_int(test_val)
     bit_array.lshift(1)
@@ -153,7 +136,6 @@ def test_bitarray_lshift():
 
 
 def test_bitarray_rshift():
-    """"""
     test_val = 15
     bit_array = BitArray.from_int(test_val)
     bit_array.rshift(1)
